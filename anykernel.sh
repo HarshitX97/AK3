e09a85e987c5efd28786c26a456f17155f603013
iitd19ishita <LecCmD@protonmail.com>
2022-12-20 18:42:50 +0530

    Adding AK3 Files for HYDRA Level 7

---
+++ anykernel.sh
@@ -0,0 +1,44 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=HYDRA-Kernels-Test 
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=begonia
+device.name2=begoniain
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+no_block_display=true;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## begin vendor changes
+mount -o rw,remount -t auto /vendor >/dev/null;
+
+# Make a backup of init.target.rc
+restore_file /vendor/etc/init/hw/init.target.rc;
+
+# Clean up other kernels' ramdisk overlay files
+rm -rf $ramdisk/overlay;
+rm -rf $ramdisk/overlay.d;
+
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
