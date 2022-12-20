8d0455612e34a248edc2700e456220f8bac62fd4
Harshit Aggarwal <LecCmD@protonmail.com>
2022-12-20 18:45:05 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=HYDRA-Kernels-Test 
+kernel.string=HYDRA-Kernels by @HarshitX97 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
